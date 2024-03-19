9439b82fd82f2652779e99778076e9bbbc0882a2
Sebastian Pająk <42960067+Sebastian2103@users.noreply.github.com>
2024-03-19 22:28:31 +0100

    Koniec!!! 💐

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,6 +5,11 @@ from django.urls import reverse
 from taggit.managers import TaggableManager
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Roboczy'
@@ -23,7 +28,7 @@ class Post(models.Model):
                               default=Status.DRAFT)
     objects = models.Manager()
     tags = TaggableManager()
-
+    published = PublishedManager()
     class Meta:
         ordering = ['-publish']
         indexes = [
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,13 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post, Comment
 from django.views.decorators.http import require_POST
-from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostsForm, CommentForm
+from .forms import EmailPostsForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
+
 
 
 # class PostListView(ListView):
@@ -104,3 +105,18 @@ def post_comment(request, post_id):
         comment.save()
     return render(request, 'blog/post/comment.html',
                   {'post': post, 'form': form, 'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
+
